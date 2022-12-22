b3b9e516f36eb4932b8bb00df1d0d1b3fec696c0
E0SelmY4V-PC <2507917746@qq.com>
2022-12-22 17:58:12 +0800

    Add 签名

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,24 @@
-export * as lang from './lang';
-export * as file from './file';
+import * as lang from './lang';
+import * as file from './file';
+/**
+ * |简·陋| Node服务器 - 实用函数
+ * @version 1.20221222.0
+ * @author E0SelmY4V
+ * @license GPL-3.0-or-later
+ * @link https://github.com/E0SelmY4V/prac-fn
+ */
+declare module "prac-fn"
+export {
+	/**
+	 * |简·陋| - 各种方便好用的 JS 功能函数
+	 * @version 1.20221222.0
+	 * @author E0SelmY4V
+	 */
+	lang,
+	/**
+	 * |简·陋| - 各种方便好用的文件操作函数
+	 * @version 1.20221222.0
+	 * @author E0SelmY4V
+	 */
+	file,
+}
